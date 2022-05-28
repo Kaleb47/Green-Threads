9a180de321373343ec827334cc07403c1d6a9db3
Killuminati <63081938+Kaleb47@users.noreply.github.com>
2022-05-28 17:56:23 +0000

    Initial commit

---
+++ target/debug/deps/my_project-97f11c63041cf46e.d
@@ -0,0 +1,5 @@
+/home/runner/Green-Threads/target/debug/deps/my_project-97f11c63041cf46e.rmeta: src/main.rs
+
+/home/runner/Green-Threads/target/debug/deps/my_project-97f11c63041cf46e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/my-project.d
@@ -0,0 +1,1 @@
+/home/runner/Green-Threads/target/debug/my-project: /home/runner/Green-Threads/src/main.rs
